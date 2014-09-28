7d68fbd0b869424d395f72f26e17281df4665a29
Mikael Brevik <mikaelbre@gmail.com>
2014-09-28 22:02:52 +0200

    Adds CLI examples

---
+++ del1/exercises/part2/commandline_examples.js
@@ -0,0 +1,22 @@
+// Try out the different NPM CLI options:
+
+
+// Make a new module:
+//> npm init
+
+// Fill out with some arbitrary data
+
+
+// Install some dependencies
+//> npm install --save stringywingy
+
+// Install another dependency, with short-cut:
+//> npm i -S underscore.string
+
+
+// Try to list what dependencies you have:
+//> npm ls
+
+
+// You can see if you have any outdated packages:
+//> npm outdated
